d29104ef8d4bd41f8ba941279e24dc2c315ee86b
jneira <atreyu.bbb@gmail.com>
2010-02-19 10:58:03 +0100

    'Blog2'

--- mult-listas.clj
+++ mult-listas.clj
@@ -1,4 +1,5 @@
-(ns mult-listas)
+(ns mult-listas
+  (:use clojure.contrib.seq-utils)))
 
 (defn div [x y] [(quot x y) (rem x y)])
 
@@ -11,3 +12,16 @@
 
 (assert (= (list 1 2 3 4 5) (numList 12345)))
 (assert (= 12345 (listNum '(1 2 3 4 5))))
+
+(defn sumListAc [acc x]
+   (let [s (+ x (acc 0)) [d r] (div s 10)]
+     [d (cons r (acc 1))]))
+(defn multList [m lst]
+     (let [[x,xs] (reduce sumListAc [0 '()]
+                              (reverse (map #(* %1 m) lst)))]
+      (if (zero? x) xs (flatten [x,xs]))))
+(defn multLists [xs ys]
+     (reduce #(cons (multList %2 '(4 5 6)) %1) '() '(4 5)))
+
+(multList 2 '(5 6 7 8))
+(multLists '(5 6 7 8) '(1 2))
