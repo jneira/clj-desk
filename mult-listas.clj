b844d61ac488fed095e84624cd8325c940a58d00
unknown <atreyu@.(none)>
2010-02-21 23:56:45 +0100

    blog8

--- mult-listas.clj
+++ mult-listas.clj
@@ -13,19 +13,19 @@
 (assert (= (list 1 2 3 4 5) (numList 12345)))
 (assert (= 12345 (listNum '(1 2 3 4 5))))
 
-(defn sumCarry 
+(defn carry 
   [acc x] (let [s (+ x (acc 0)) 
 		[d r] (div s 10)]
 	    [d (cons r (acc 1))]))
 
-(defn sumListCarry [xs] 
-  (let [[x,xs] (reduce sumCarry [0 []] (reverse xs))]
-       (if (zero? x) xs (flatten [x,xs]))))
+(defn listCarry [xs] 
+  (let [[x,xs] (reduce carry [0 []] (reverse xs))]
+       (if (zero? x) xs (cons x xs))))
 
-(assert (= [1 '(5)] (sumCarry [0 ()] 15)))
-(assert (= [1 '(4 5)] (sumCarry [1 '(5)] 13)))
-(assert (= [1 '(2 4 5)] (sumCarry [1 '(4 5)] 11)))
-(assert (= '(1 2 4 5) (sumListCarry '(11 13 15))))
+(assert (= [1 '(5)] (carry [0 ()] 15)))
+(assert (= [1 '(4 5)] (carry [1 '(5)] 13)))
+(assert (= [1 '(2 4 5)] (carry [1 '(4 5)] 11)))
+(assert (= '(1 2 4 5) (listCarry '(11 13 15))))
 
 (defn lpad [xs x y] (concat (replicate x y) xs)) 
 (defn maxLength [& lst] (apply max (map count lst)))
@@ -41,14 +41,14 @@
 (assert (= '((1 2 3) (1 2 0) (1 0 0)) (multDespl '((1 2 3) (1 2) (1)))))
 
 (defn multList [m lst]
-     (sumListCarry (map #(* %1 m) lst)))
+     (listCarry (map #(* %1 m) lst)))
 
 (defn multLists [xs ys]
      (let [mults (reduce #(cons (multList %2 xs) %1) '() ys)
 	   despl (multDespl mults)
 	   max (apply maxLength despl)
 	   lpaded (map #(lpad %1 (- max (count %1)) 0) despl)]
-       (sumListCarry (apply map + lpaded))))
+       (listCarry (apply map + lpaded))))
 
 (assert (= 11356 (listNum (multList 2 '(5 6 7 8)))))
 (assert (= 698394 (listNum (multLists '(5 6 7 8) '(1 2 3)))))
