d41662d0e404dfdd7efc0e39b710e2a837b5dfd3
unknown <atreyu@.(none)>
2010-07-13 22:03:42 +0200

    ''

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clj-desk "1.0.0-SNAPSHOT"
+  :description "A project to learn and experiment with clojure"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]])
