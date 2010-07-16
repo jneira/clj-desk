add513ae26de7ff3033c35dc1ead57bd9d522ae9
unknown <atreyu@.(none)>
2010-07-16 23:45:59 +0200

    *** empty log message ***

--- src/hashiwokakero.clj
+++ src/hashiwokakero.clj
@@ -1,19 +1,24 @@
-(ns hashiwokakero)
-
-(defonce max-bridges 8)
-(defonce dirs {:north [0,-1] :east [1,0] :south [0,1] :west [-1,0]})
-
-(defstruct puzzle-node :panel :islands)
-
-
-(comment defn nthn-old [coll coords] 
-  (let [s (seq coords)
-        next (nth coll (first s))]
-    (if (= (count s) 1) next
-        (recur next (rest coords)))))
+(ns hashiwokakero
+  (:require (clojure.contrib [combinatorics :as comb])))
 
 (defn nthn [coll coords]
   (reduce nth coll coords))
+
+(defn backtracking [solution? explore node]
+  (if (solution? node) [node]
+      (mapcat
+       #(backtracking solution? explore %1)
+       (explore node))))
+
+(defonce max-bridges 8)
+(defonce directions
+  {:north [0,-1] :east [1,0] :south [0,1] :west [-1,0]})
+
+(defstruct puzzle-node :panel :islands)
+(defstruct coords :x :y)
+(defstruct bridge :direction :value)
+(defstruct island :coords :bridges)
+
 
 (defn squared? [p]
   (let [length (count p)
@@ -25,9 +30,22 @@
         length (count (first with0))] 
     (interpose (replicate length 0) with0)))
 
-(defn init-islands [p]
+(defn get-islands [p]
   (for [x (range 0 (count p))
         y (range 0 (count (nth p x)))
-        :when (> (-> p (nth x) (nth y)) 0)]
-    [x,y]))
+        :when (> (nthn p [x y]) 0)]
+    (struct island (struct coords x y))))
 
+(defn valid-bridges? [panel island bridges]
+  nil)
+
+(defn get-bridges
+  ([vals] (get-bridges (keys directions) vals))
+  ([dirs vals] (map #(struct bridge %1 %2) dirs vals)))
+
+(defn init-bridges [maxTotal maxByBridge]
+  (map  #(get-bridges %1)
+        (filter #(= maxTotal (apply + %1))
+                (comb/selections (range 0 (inc maxTotal)) 4))))
+
+(defn limit-num-bridges? [limit] nil)
