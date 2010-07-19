2a9bb270f2b099f4049c845d3b88f76db8ab013c
unknown <atreyu@.(none)>
2010-07-19 06:31:03 +0200

    *** empty log message ***

--- src/hashiwokakero.clj
+++ src/hashiwokakero.clj
@@ -4,6 +4,19 @@
 (defn nthn [coll coords]
   (reduce nth coll coords))
 
+(defn in-range? [vec coords]
+  (boolean
+   (reduce #(and
+             (vector? %1)
+             (> (count %1) %2)
+             (%1 %2))
+           vec coords)))
+
+(defn squared? [p]
+  (let [length (count p)
+        lengths (map count p)]
+    (apply = length lengths)))
+
 (defn backtracking [solution? explore node]
   (if (solution? node) [node]
       (mapcat
@@ -19,25 +32,16 @@
 (defstruct bridge :direction :value)
 (defstruct island :coords :bridges)
 
-
-(defn squared? [p]
-  (let [length (count p)
-        lengths (map count p)]
-    (apply = length lengths)))
-
-(defn init-puzzle [p]
+(defn init-panel [p]
   (let [with0 (map #(interpose 0 %1) p)
         length (count (first with0))] 
     (interpose (replicate length 0) with0)))
 
-(defn get-islands [p]
+(defn init-islands [p]
   (for [x (range 0 (count p))
         y (range 0 (count (nth p x)))
         :when (> (nthn p [x y]) 0)]
     (struct island (struct coords x y))))
-
-(defn valid-bridges? [panel island bridges]
-  nil)
 
 (defn get-bridges
   ([vals] (get-bridges (keys directions) vals))
@@ -46,6 +50,25 @@
 (defn init-bridges [maxTotal maxByBridge]
   (map  #(get-bridges %1)
         (filter #(= maxTotal (apply + %1))
-                (comb/selections (range 0 (inc maxTotal)) 4))))
+                (map #(filter (complement zero?) %1)
+                     (comb/selections (range 0 (inc maxByBridge)) 4)))))
 
-(defn limit-num-bridges? [limit] nil)
+(defn valid-bridges? [panel island bridges]
+  nil)
+
+(defn init-root-node [p k]
+  (let [panel (init-panel p)]))
+
+(defn next-bridge-extend [bridge coords]
+  (let [dir (directions (direction bridge))]
+    (map + coords dir)))
+
+(defn extend-bridge [panel cell bridge]
+  (letfn
+      (step [exts coord]
+            (let [dir (directions (direction bridge))
+                  nextCell (map + cell dir)
+                  nextVal (panel (:x cell) (:y cell))]
+              (cond
+               ((complement in-range?) p nextCell) [nil :out-of-range]
+               (< nextVal 0) (if ))))))
