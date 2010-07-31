a71a9aa515054f534c89cf9416a2895041a4fe60
unknown <atreyu@.(none)>
2010-08-01 01:20:35 +0200

    *** empty log message ***

--- src/hashiwokakero.clj
+++ src/hashiwokakero.clj
@@ -9,20 +9,15 @@
              (%1 %2))
            vec coords)))
 
-(defn squared? [p]
-  (let [length (count p)
-        lengths (map count p)]
-    (apply = length lengths)))
-
 (defn backtracking [solution? explore node]
   (if (solution? node) [node]
       (mapcat
        #(backtracking solution? explore %1)
        (explore node))))
 
-(defonce max-bridges 8)
-(defonce directions
-  {:north [0,-1] :east [1,0] :south [0,1] :west [-1,0]})
+(def max-bridges 8)
+(def directions
+  {:north [-1,0] :east [0,1] :south [1,0] :west [0,-1]})
 
 (defstruct node :panel :islands)
 (defstruct coords :x :y)
@@ -63,40 +58,51 @@
                (directions direction))]
     (apply struct coords (map + [x y] dir))))
 
-(defn next-bridge-step [bridge cell]
-  (let [dir (directions (:direction bridge))]
-    (move cell dir)))
-
 (defn num-bridges [panel island]
   (let [mv&get #(get-value panel (move island %1))
-        cells (map mv&get directions)]
-    cells))
+        cells (map mv&get (keys directions))]
+    (apply + (remove nil?  cells))))
 
-(defn fit-bridge [panel cell val]
-  )
+(defn left-bridges [panel island]
+  (+ (get-value panel island) (num-bridges panel island)))
+
+(defn fit-bridge [panel island bridge-val]
+  (let [new-bridges
+        (- (left-bridges panel island) bridge-val)]
+    (>= new-bridges 0)))
+
+(defn next-bridge-step [cell bridge]
+  (let [dir (:direction bridge)]
+    (move cell dir)))
 
 (defn extend-bridge [panel cell bridge]
   (letfn
-      [(step [prevCell]
+      [(step [prevCell acc]
              (let [val (:value bridge)
-                   nextCell (next-bridge-step bridge prevCell)
-                   nextVal (get-val panel nextCell)]
+                   nextCell (move prevCell (:direction bridge))
+                   nextVal (get-value panel nextCell)]
+               (println val nextCell nextVal acc)
                (cond
-                ((complement in-range?) panel nextCell) :out-of-range
+                (nil? nextVal)    :out-of-range
                 (< nextVal 0) (if (= cell prevCell)
-                                :cross-other-bridge
+                                  :cross-other-bridge
                                 (if (= nextVal val)
                                   :match-prev-bridge
                                   :mismatch-prev-bridge))
                 (> nextVal 0) (if (fit-bridge panel nextCell val)
-                                nextCell
-                                :island-full)
-                (= nextVal 0) (recur nextCell))))]
-    (step cell)))
+                                acc
+                                  :island-full)
+                (= nextVal 0)  (recur nextCell (conj acc nextCell)))))]
+    (step cell [])))
+
+(defn put-bridges [panel island bridges]
+  (let [exts (map #(extend-bridge panel island %1) bridges)]
+    (when (not-any? keyword? exts)
+      (reduce  ))))
 
 (defn valid-bridges? [panel island bridges]
   nil)
 
 
-(def node (init-root-node [[0 2 0] [2 2 2] [0 2 0]]))
+(def node (init-root-node [[0 2 0] [0 0 0] [2 2 2] [0 2 0]]))
 
