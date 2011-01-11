c243addb12495299b3dfe48fc2a2d6a43f17a580
jneira <areyu.bbb@google.com>
2011-01-11 23:57:36 +0100

    *** empty log message ***

--- test/clj_desk/romans_numerals_test.clj
+++ test/clj_desk/romans_numerals_test.clj
@@ -1,10 +1,21 @@
 (ns roman-numerals-test
   (:use roman-numerals clojure.test))
 
+
+
 (deftest conversion-from-int-to-roman
+
+  (defn repeat-str [s n]
+    (apply str (repeat n s)))
+  
   (defn to-roman [n]
-    (apply str (repeat n "I")))
+    (cond
+     (> n 4) (str "V"
+                  (apply str (repeat (mod n 5) "I")))
+     (= n 4) "IV"
+     (< n 4) (apply str (repeat n "I"))     ))
   (are [x y]  (= (to-roman x) y)
-       1 "I" 2 "II" 3 "III" 4 "IV"))
+       1 "I" 2 "II" 3 "III" 4 "IV" 5 "V"
+       6 "VI" 9 "IX"))
 
 (run-tests)
