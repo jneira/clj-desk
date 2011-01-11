b71a6921129ac25fbf8c102c71952811a71b4512
unknown <atreyu@.(none)>
2011-01-11 21:08:22 +0100

    'refactor 2'

--- test/clj_desk/romans_numerals_test.clj
+++ test/clj_desk/romans_numerals_test.clj
@@ -2,7 +2,10 @@
   (:use roman-numerals clojure.test))
 
 (deftest conversion-from-int-to-roman
-  (defn to-roman [n] "I")
+  (defn to-roman [n] (case n 1 "I" 2 "II"))
   (testing "1 must be I"
-    (is (= (to-roman 1) "I"))))
+    (is (= (to-roman 1) "I")))
+  (testing "2 must be II"
+    (is (= (to-roman 2) "II"))))
 
+(run-tests)
