370df29e9c54238d620e8fdbac7785b79e2e14e6
unknown <atreyu@.(none)>
2011-01-11 20:37:27 +0100

    'roman numerals baby step 1'

--- test/clj_desk/romans_numerals_test.clj
+++ test/clj_desk/romans_numerals_test.clj
@@ -2,6 +2,7 @@
   (:use roman-numerals clojure.test))
 
 (deftest conversion-from-int-to-roman
-  (defn to-roman [n])
-    (is (= (to-roman 1) "I")))
+  (defn to-roman [n] "I")
+  (testing "1 must be I"
+    (is (= (to-roman 1) "I"))))
 
