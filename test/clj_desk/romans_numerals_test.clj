4853d3a9b21b732a50441bb89df26a41f1b34f0b
unknown <atreyu@.(none)>
2011-01-11 21:13:08 +0100

    'refactor test 1'

--- test/clj_desk/romans_numerals_test.clj
+++ test/clj_desk/romans_numerals_test.clj
@@ -3,9 +3,8 @@
 
 (deftest conversion-from-int-to-roman
   (defn to-roman [n] (case n 1 "I" 2 "II"))
-  (testing "1 must be I"
-    (is (= (to-roman 1) "I")))
-  (testing "2 must be II"
-    (is (= (to-roman 2) "II"))))
+  (are [x y]  (= (to-roman x) y)
+       1 "I"
+       2 "II"))
 
 (run-tests)
