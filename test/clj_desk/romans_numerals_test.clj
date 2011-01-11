92065ec11afd0172749f1e31f8497810852fb9eb
unknown <atreyu@.(none)>
2011-01-11 21:15:54 +0100

    'pass 3'

--- test/clj_desk/romans_numerals_test.clj
+++ test/clj_desk/romans_numerals_test.clj
@@ -2,7 +2,8 @@
   (:use roman-numerals clojure.test))
 
 (deftest conversion-from-int-to-roman
-  (defn to-roman [n] (case n 1 "I" 2 "II"))
+  (defn to-roman [n]
+    (case n 1 "I" 2 "II" 3 "III"))
   (are [x y]  (= (to-roman x) y)
        1 "I"
        2 "II"
