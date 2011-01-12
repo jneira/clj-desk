a943365bcde927067283f050915c24e748d569f1
jneira <atreyu.bbb@gmail.com>
2011-01-12 17:12:48 +0100

    commit

--- test/clj_desk/romans_numerals_test.clj
+++ test/clj_desk/romans_numerals_test.clj
@@ -5,17 +5,19 @@
 
 (deftest conversion-from-int-to-roman
 
-  (defn repeat-str [s n]
+  (defn repeat-str [n s]
     (apply str (repeat n s)))
   
   (defn to-roman [n]
     (cond
-     (> n 4) (str "V"
-                  (apply str (repeat (mod n 5) "I")))
+     (> n 9) (str "X" (to-roman (- n 10)))
+     (= n 9) "IX"
+     (> n 4) (str "V" (repeat-str (mod n 5) "I"))
      (= n 4) "IV"
-     (< n 4) (apply str (repeat n "I"))     ))
+     (< n 4) (repeat-str n "I")))
   (are [x y]  (= (to-roman x) y)
        1 "I" 2 "II" 3 "III" 4 "IV" 5 "V"
-       6 "VI" 9 "IX"))
+       6 "VI" 9 "IX" 10 "X" 14 "XIV" 19 "XIX"
+       39 "XXXIX" 40 ))
 
 (run-tests)
