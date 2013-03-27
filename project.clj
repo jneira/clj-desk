39f20f5da8108c43b0ae31f03f751cfd1dd6c175
jneira <atreyu.bbb@gmail.com>
2013-03-27 14:43:11 +0100

    test

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-desk "1.0.0-SNAPSHOT"
   :description "A project to learn and experiment with clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/core.logic "0.6.4"]
                  [trammel "0.5.0"]
                  [midje "1.3-alpha4"]])
