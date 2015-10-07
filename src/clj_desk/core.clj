e22ef56d8b6ca10816b9c9ecfbcfc6c29064ece1
jneira <atreyu.bbb@gmail.com>
2015-10-07 22:04:01 +0200

    cleaning

--- src/clj_desk/core.clj
+++
@@ -1,9 +0,0 @@
-(ns clj-desk.core
-  (use clojure.walk))
-
-(defn f [x] (* x 2))
-
-(defmacro dup [f]
-  (let [[f1 args body] f
-        r #(if ((set args) %) `(+ ~% ~%) % )]
-    `(~f1 ~args ~(postwalk r body))))
